979aeb4204bdfba78b7bf59f12bddb38f32d3daf
yukisaito <saitouyuuki@yukisaitos-MacBook-Pro.local>
2022-07-10 16:42:36 +0900

    modified:   sql/select.sql

--- sql/select.sql
+++ sql/select.sql
@@ -9,17 +9,26 @@ select * from orders limit 10;
 # テーブルにも付ける事ができる
 select (tax + tax) as 2tax from orders limit 10;
 
+# 演習
+Product_id、quantitiy、taxを使ってtotalカラムと数値が一致することを確認してみよう
 
 # 取得するカラムを設定できる
 select id,tax from orders limit 10;
 
 # 条件付き
 select * from orders where id = 74
+# 値の比較もできます
 select * from orders where created_at > '2018-11-11'
 # 複数条件(and 且つ条件)
 select * from orders where id > 70 and tax < 4
 # 複数条件(or または条件)
 select * from orders where id > 70 or tax < 4
+# Like
+# %はなんでもありという意味
+select Product_id from orders where Product_id like '%36%'
+
+# 演習
+Product_id、quantitiy、taxを使ってtotalカラムと数値が一致したらTrueを別名カラムで出力してみよう
 
 # with
 # 繰り返し行うような処理におすすめです
@@ -43,3 +52,111 @@ select * from orders
 where id in (
     select id from orders where id % 2 = 0 and id < 10
 )
+
+# Case分
+# もしXXXであったら？というように条件分岐をする事ができる
+
+select 
+    CASE id
+        WHEN 1 THEN 'hoge'
+        WHEN 3 THEN 'peke'
+        ELSE 'gggggg'
+    END as peken
+    , id
+from
+ orders
+where
+ id in(1,2,3)
+
+# 演習
+product_idが100以上だったら、Product_id、quantitiy、taxを利用してtotalを計算し、
+そうでなければ、100を固定で入れるようにしてみよう
+
+# 複数テーブルを操作する join
+# inner joinとleft join
+
+# 集計関数(Group By)
+# Excelでいう小計であったり合計を出したりすることに使う
+# order byは並び替えの方法ascは昇順(小 -> 大)、descは逆
+
+#以下はuserごとに購入金額の合計を出すSQL
+select  
+    USER_ID,
+    sum(total) as user_sum
+from 
+    ORDERS
+group by USER_ID
+order by user_sum asc
+
+# 演習
+product_idごとに集計し、最も売れていないProduct_idを絞り出そう
+select  
+    PRODUCT_ID,
+    sum(total) as product_sum
+from 
+    ORDERS
+group by PRODUCT_ID
+order by product_sum asc
+
+# 欠損処理
+# null 除外
+# null を別の値で置き換える
+# 先ほどのleft joinとくっつけると強力
+
+# 擬似的に欠損データを作ります
+# こちらが新たなorders テーブルだと一時的に思ってください
+select * from (
+select 
+    CASE id
+        WHEN 1 THEN '123'
+        WHEN 3 THEN 'peke'
+        ELSE null
+    END as peken
+    , product_id
+from
+ orders
+where
+ id in(1,2,3)
+ ) as orders
+# COALESCEを使って穴埋めしてきます
+select COALESCE(orders.peken, cast(orders.product_id as character)), orders.product_id from (
+select 
+    CASE id
+        WHEN 1 THEN '123'
+        WHEN 3 THEN 'peke'
+        ELSE null
+    END as peken
+    , product_id
+from
+ orders
+where
+ id in(1,2,3)
+ ) as orders
+
+# 桁合わせ
+# product sumが桁が揃っていなくてわかりずらいので分析の際には桁数などの「揺れ」を統一させることが大事
+select  
+    PRODUCT_ID,
+    round(sum(total),1) as product_sum
+from 
+    ORDERS
+group by PRODUCT_ID
+order by product_sum asc
+# 他にも「1」「男」みたいな状況であれば、Case文を使って揺れを統一させていきます
+# 非常に骨の折れる作業ですが使いやすさのため、欠損処理も含めとっても大切な作業です
+
+
+# 重複削除
+
+select distinct(orders.peken) from (
+select 
+    CASE id
+        WHEN 1 THEN '123'
+        WHEN 3 THEN 'peke'
+        ELSE '123'
+    END as peken
+from
+ orders
+where
+ id in(1,2,3)
+ ) as orders
