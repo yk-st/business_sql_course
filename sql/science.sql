d6facddf8a1e4d292a6ea10aa308c50552f14104
yukisaito <saitouyuuki@yukisaitos-MacBook-Pro.local>
2022-07-21 21:41:32 +0900

    modified:   sql/science.sql

--- sql/science.sql
+++ sql/science.sql
@@ -1,18 +1,3 @@
-月ごとの分析
-with hoge as (
-    select * from (
-        SELECT product_id,total,to_char(created_at, 'YYYY-MM') AS sa_month
-        FROM orders
-    )  peke
-)
-select 
-    sa_month,sum(total),product_id
-from hoge 
-group by sa_month,product_id
-order by product_id,sa_month
-;
-
-
 # デシル分析
 # ユーザの購入ランクにてランク分けして、10グループに分けてみましょう
 # 均等に１０分割できるようにntileを利用して分割します
@@ -68,16 +53,71 @@ select
 from 
 math_base
 
- 
-
 # ABC分析
+# 売れ筋の商品をA~Cランクに分ける
 # Aランク 0~70
 # Bランク 70~90
 # Cランク 90~10
-# ロイヤルユーザとか言ったりする
+
+with hoge as (
+    select * from (
+        SELECT product_id,total,to_char(created_at, 'YYYY-MM') AS sa_month
+        FROM orders
+    )  peke
+)
+,
+base_data as (
+select 
+    sum(total) as amount,product_id, prod.title
+from hoge 
+inner join products as prod
+on product_id = prod.id
+group by product_id,title
+order by amount
+)
+
+-- 売上における構成費を出力する
+,composition as (
+select
+title,
+-- プロダクトごとの売上
+sum(amount) as product_amount,
+-- 全体の売上 
+sum(sum(amount)) over() as total_amount,
+100.0 * sum(amount) / sum(sum(amount)) over() as composition_ratio
+from base_data
+group by title
+order by composition_ratio desc
+)
+,
+-- 累計割合を出す
+cumulative as (
+select 
+ title,
+ product_amount,
+ composition_ratio,
+ sum(composition_ratio) over(order by composition_ratio desc) as cumulative_ratio
+from composition
+)
+
+-- ランク分けを行う
+SELECT
+ title,
+ product_amount,
+ composition_ratio,
+ cumulative_ratio,
+ CASE
+     WHEN cumulative_ratio between 0 and 70 then 'A'
+     WHEN cumulative_ratio between 70 and 90 then 'B'
+     WHEN cumulative_ratio between 90 and 101 then 'C'
+ END as abc_rank
+ FROM
+ cumulative
 
 # RFM分析
 # デシル分析のさらに細かいバージョン
+
+
 
 # ファンチャート
 # とある時点を100として
@@ -114,8 +154,57 @@ select
 
 # Zチャート
 # 月次売上/売上累計/移動年計
+# 当該月を2017/11月にしましょう
+
+# ステップ１：月毎に集計します
+# ファンチャートのSQLをそのまま使います
+# ステップ２：次ごとの累計を少しづつ足していきます。
 # 各合計をひと月づつずらしていくイメージです。
 # 移動年計は当該月の売上と過去11ヶ月の売上累計を足し合わせます
+with hoge as (
+    select * from (
+        SELECT product_id,total,to_char(created_at, 'YYYY-MM') AS sa_month
+        FROM orders
+    )  peke
+)
+,
+-- 月ごとの集計
+-- 当該月を2018-01として行い、2017年のZチャートを作成したい
+-- 移動年計を出すためには2016-01月からデータが必要
+base_data as (
+select 
+    sa_month,sum(total) as month_amount
+from hoge 
+inner join products as prod
+on product_id = prod.id
+where 
+ sa_month between '2016-01' and '2017-12'
+group by sa_month
+order by sa_month
+)
+
+, nenkei as (
+-- 月累計を行う
+select 
+
+sa_month,month_amount as "月次",
+-- 2017のZチャートにしたいので条件をつける
+sum(case when sa_month like '%2017%' then month_amount end) over( order by sa_month ) as "累計",
+sum(month_amount) over(order by sa_month , sa_month rows between 11 preceding and CURRENT ROW) as "移動"
+
+from 
+base_data
+)
+
+select 
+* 
+from 
+nenkei
+where
+sa_month like '%2017%'
+order by sa_month
+
+# プロダクトごとに集計することも可能です
 
 # アソシエーション分析
 # 最後に紹介するのはデータマイニングの一種、データを探索していくという意味
@@ -133,13 +222,8 @@ select
 #　一般にリフトが1以上が良いと言われています
 
 # ユークリッド距離とコサイン類似度
+# 似ている似ていないを判断することに使える
 select
  sqrt(power(x1-x2,2) + power(y1-y2,2)) as dist
 from
  location
-
-# User 2 Item
-# 好き嫌い判断などに使う事ができる
-# ユーザー間で類似性を判定し
-# 例えば、肉カウント、魚カウントから定量的なスコアを出す
-# 例えば、他のユーザの購入履歴からあなたへのおすすめを出す
